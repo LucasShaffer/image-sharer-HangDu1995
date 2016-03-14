7fff9e697c8062d742aa6970606c01a1af7be442
Bryce Boe <bbzbryce@gmail.com>
2016-03-14 10:16:27 -0700

    Add a restrictive content security policy header to all requests.
    A strong content security policy can be used to prevent XSS and other
    attacks. With many things security related, it is a good idea to disallow
    everything and then allow things only after questioning whether or not they
    should be allowed (a whitelist policy).
    
    The most restrictive content security policy is:
    
        Content-Security-Policy: default-src 'none';
    
    However this does not permit any stylesheets, images, nor javascript sources
    all of which we want to be able to serve up locally, but not inline. The
    following accomplishes that task:
    
        Content-Security-Policy: default-src 'none'; script-src 'self'; img-src 'self'; style-src 'self';
    
    A few modifications have been made to that policy to allow bootstrap to work.
    
    https://www.owasp.org/index.php/Content_Security_Policy_Cheat_Sheet

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,18 @@ module Base
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # unsafe-eval is needed for script-src due to bootstrap
+    # There is an outstanding issue to hopefully make that unnecessary:
+    # https://github.com/twbs/bootstrap/issues/17964
+    csp_settings = ["connect-src 'self'",
+                    "default-src 'none'",
+                    'font-src https://cdn.jsdelivr.net',
+                    "img-src 'self' http: https:",
+                    "script-src 'self' 'unsafe-eval'",
+                    "style-src 'self' https://cdn.jsdelivr.net"]
+    config.action_dispatch.default_headers = {
+      'Content-Security-Policy' => csp_settings.join(';')
+    }
   end
 end
