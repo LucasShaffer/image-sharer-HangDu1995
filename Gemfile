6cf8b97ca26b27892eb52bee90455737de92859d
Bryce Boe <bbzbryce@gmail.com>
2016-07-19 11:57:00 -0700

    Add rubocop with config and fix its warnings.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 source 'https://rubygems.org'
 ruby '2.3.0'
 
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0'
 # Use Puma as the app server
@@ -39,6 +38,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'pronto-rubocop'
 end
 
 group :production do
--- test/flow_test_helper.rb
+++ test/flow_test_helper.rb
@@ -12,12 +12,14 @@ class FlowTestCase < ActiveSupport::TestCase
 end
 
 # Tip 3 from http://blog.plataformatec.com.br/2011/12/three-tips-to-improve-the-performance-of-your-test-suite/
-class ActiveRecord::Base
-  mattr_accessor :shared_connection
-  @@shared_connection = nil
+module ActiveRecord
+  class Base
+    mattr_accessor :shared_connection
+    self.shared_connection = nil
 
-  def self.connection
-    @@shared_connection || retrieve_connection
+    def self.connection
+      shared_connection || retrieve_connection
+    end
   end
 end
 
