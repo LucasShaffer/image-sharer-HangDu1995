b4c9f3cff7b1d77efb7ea783f306b0bbb6c91162
Bryce Boe <bbzbryce@gmail.com>
2018-01-03 11:23:14 -0800

    Add webpacker info for es6 support

---
+++ app/javascript/delete_me.js
@@ -0,0 +1,8 @@
+class DeleteMe {
+  constructor() {
+    this.message = 'This message is from delete_me.js. Delete this file when implementing share image.'
+  }
+}
+
+const deleteMe = new DeleteMe();
+console.log(`message: ${deleteMe.message}`);
