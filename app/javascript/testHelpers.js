8cc5140a1b2031f8a07d021f7f611c3306b335c9
Darren Eng <darren.eng@appfolio.com>
2019-03-11 11:45:22 -0700

    de - add test helper to script

---
+++ app/javascript/testHelpers.js
@@ -0,0 +1,23 @@
+/* eslint import/no-extraneous-dependencies: 1 */
+
+import 'jsdom-global/register';
+import Adapter from 'enzyme-adapter-react-16/build/index';
+import { configure } from 'enzyme';
+
+configure({ adapter: new Adapter() });
+
+//
+// Throw exceptions on unhandled promise rejections to prevent tests
+// from silently failing async.
+//
+let unhandledRejection = false;
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('unhandled rejection:', reason || promise); // eslint-disable-line no-console
+  unhandledRejection = true;
+  throw promise;
+});
+process.prependListener('exit', (code) => {
+  if (unhandledRejection && code === 0) {
+    process.exit(1);
+  }
+});
