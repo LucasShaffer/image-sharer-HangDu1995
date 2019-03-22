e5ab51186bc84ba7d7c03286a64d0bdad10f3076
Darren Eng <darren.eng@appfolio.com>
2019-03-22 15:39:47 -0700

    de - move CSP to content_security_policy

--- config/application.rb
+++ config/application.rb
@@ -12,21 +12,6 @@ module Base
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # unsafe-eval is needed for script-src due to bootstrap
-    # There is an outstanding issue to hopefully make that unnecessary:
-    # https://github.com/twbs/bootstrap/issues/17964
-    csp_settings = [
-      "connect-src 'self'",
-      "default-src 'none'",
-      'font-src https://cdn.jsdelivr.net',
-      "img-src 'self' http: https:",
-      "script-src 'self' https://cdnjs.cloudflare.com https://code.jquery.com https://maxcdn.bootstrapcdn.com",
-      "style-src 'self' https://maxcdn.bootstrapcdn.com"
-    ]
-    config.action_dispatch.default_headers = {
-      'Content-Security-Policy' => csp_settings.join(';')
-    }
-
     config.active_record.sqlite3.represent_boolean_as_integer = true
   end
 end
---
+++ config/initializers/content_security_policy.rb
@@ -0,0 +1,30 @@
+# Be sure to restart your server when you modify this file.
+
+# Define an application-wide content security policy
+# For further information see the following documentation
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+
+Rails.application.config.content_security_policy do |policy|
+  policy.connect_src :self, :https
+  policy.default_src :self, :https
+  policy.font_src    :self, :https, :data, 'https://cdn.jsdeliv.net'
+  policy.img_src     :self, :http, :https, :data
+  policy.object_src  :none
+
+  # unsafe-eval is needed for script-src due to bootstrap
+  # There is an outstanding issue to hopefully make that unnecessary:
+  # https://github.com/twbs/bootstrap/issues/17964
+  policy.script_src  :self, :http, 'https://cdnjs.cloudflare.com', 'https://code.jquery.com', 'https://maxcdn.bootstrapcdn.com'
+  policy.style_src   :self, :https, 'https://maxcdn.bootstrapcdn.com'
+
+  # Specify URI for violation reports
+  # policy.report_uri "/csp-violation-report-endpoint"
+end
+
+# If you are using UJS then enable automatic nonce generation
+# Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
+
+# Report CSP violations to a specified URI
+# For further information see the following documentation:
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
+# Rails.application.config.content_security_policy_report_only = true
