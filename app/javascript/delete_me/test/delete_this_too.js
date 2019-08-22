3085e86dabf3eb9ee254bd2228e6112917262357
Darren Eng <darren.eng@appfolio.com>
2019-08-22 14:21:26 -0700

    de - add test so that async await doesn't break again in the future

--- app/javascript/delete_me/test/delete_this_too.js
+++ app/javascript/delete_me/test/delete_this_too.js
@@ -6,4 +6,9 @@ describe('delete_me', () => {
   it('passes the tests but should be deleted when you start working on this', () => {
     assert(true);
   });
+
+  it('works with async and await', async () => {
+    await 1;
+    assert(true);
+  });
 });
