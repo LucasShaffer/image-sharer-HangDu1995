9a81497c17bc400aef42470498d5bea0a7021427
Hang Du <hang.du@appfolio.com>
2019-09-05 13:47:09 -0700

    issue2 finished without tests

--- Gemfile
+++ Gemfile
@@ -47,3 +47,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
+
+# validate url
+gem "validate_url"
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_09_05_165137) do
+
+  create_table "images", force: :cascade do |t|
+    t.text "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
