1a80dcbcc4d07075b1d6291c78b4850dd9b88518
HangDu <31265902+HangDu1995@users.noreply.github.com>
2019-09-05 14:32:57 -0700

    Update welcome_controller_test.rb
    assert_response: success -> assert_response 200 
    be precise

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
     get welcome_index_url
-    assert_response :success
+    assert_response 200
   end
 end
