92c41cc75dbac5f08b8210e167c85c155d108952
Hang Du <hang.du@appfolio.com>
2019-09-19 10:15:56 -0700

    fix problems in PR for issue9

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
-  resources :images, only: %i[show new create index destroy edit]
+  resources :images, only: %i[show new create index destroy edit update]
 
   root 'images#index'
-
-  patch '/images/:id', to: 'images#update', as: 'update_image'
 end
